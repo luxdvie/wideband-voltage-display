3639656d9148f739cacaa996ffd4397a779041dd
Austin Brown <austinbrown2500@gmail.com>
2020-05-07 10:47:59 -0400

    Add basic voltage reading sketch
    This sketch will read from A0 on the Arduino and report a value between
    0.00 and 5.00. It writes on a 9600 baud serial port a JSON object which
    currently contains a single property -- Voltage

--- Arduino/VoltageReader/VoltageReader.ino
+++ Arduino/VoltageReader/VoltageReader.ino
@@ -1,37 +1,46 @@
 /*
-  Blink
-
-  Turns an LED on for one second, then off for one second, repeatedly.
-
-  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
-  the correct LED pin independent of which board is used.
-  If you want to know what pin the on-board LED is connected to on your Arduino
-  model, check the Technical Specs of your board at:
-  https://www.arduino.cc/en/Main/Products
-
-  modified 8 May 2014
-  by Scott Fitzgerald
-  modified 2 Sep 2016
-  by Arturo Guadalupi
-  modified 8 Sep 2016
-  by Colby Newman
-
-  This example code is in the public domain.
-
-  http://www.arduino.cc/en/Tutorial/Blink
+  Report voltage from 0-5v to serial port
+  https://github.com/luxdvie/wideband-voltage-display
+  Austin Brown [GitHub](https://github.com/luxdvie),  [austinbrown2500@gmail.com](mailto:austinbrown2500@gmail.com)
 */
 
-// the setup function runs once when you press reset or power the board
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
+  blink(); // Blink on setup to indicate successful boot
 }
 
-// the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+  sendData();
+  delay(5);
+}
+
+// Reads the voltage on A0 and returns a float between 0.00 and 5.00
+// https://www.arduino.cc/en/Tutorial/ReadAnalogVoltage
+float readVoltage() {
+  int sensorValue = analogRead(A0);
+  float voltage = sensorValue * (5.0 / 1023.0);
+  if (voltage <= 0.00) {
+    return 0.00; // never allow a value of less than 0
+  }
+  
+  return min(voltage, 5.00); // never allow a value of greater than 5.00
+}
+
+// Writes data to the serial port
+void sendData() {
+  // Send simple JSON data now so that we can easily add new data in future
+  Serial.print("{\"Voltage\": \""); // begin JSON
+  Serial.print(readVoltage()); // actual voltage reading
+  Serial.println("\"}"); // end JSON
+}
+
+// Takes 1 second and blinks the LED: On, Off, On
+// Leaves the LED in the ON state 
+void blink() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(500);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(500);
+  digitalWrite(LED_BUILTIN, HIGH);
 }
